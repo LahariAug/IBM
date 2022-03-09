5d4ccb03c5557f6dd1ed3d39e22a3176135daadf
LahariAug <100933095+LahariAug@users.noreply.github.com>
2022-03-09 13:14:56 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,11 @@
+package com.example.Addition;
+
+public class Addition {
+
+	public static void main(String[] args)
+	{
+		int a=20,b=30,c;
+		c=a+b;
+		System.out.println("Addition of two numbers:"+c);
+	}
+}
