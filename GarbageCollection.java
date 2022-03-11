1cc23813099c982f400499cc178862729eb28fdc
LahariAug <100933095+LahariAug@users.noreply.github.com>
2022-03-11 16:51:29 +0530

    Add files via upload

---
+++ GarbageCollection.java
@@ -0,0 +1,17 @@
+package garbagecollection;
+
+public class GarbageCollection 
+{
+	public static void main(String[] args)
+	{
+		String s = new String(" hello team mates ");
+		s=null;
+		System.gc();
+		System.out.println(" end of ");
+	}
+
+	public void finalize()
+	{
+		System.out.println(" finalize is valled");
+	}
+}
