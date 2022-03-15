7c3421e4c94a6217117a6098194296fb902dcd98
LahariAug <100933095+LahariAug@users.noreply.github.com>
2022-03-15 14:08:31 +0530

    Add files via upload

---
+++ Number Operations.js
@@ -0,0 +1,17 @@
+var a=2;
+console.log("Square of a is: " + (a*a));//square of number
+
+var a=2,b=3,c=4;
+console.log("Sum of a,b,c is : " + (a+b+c));// sum of 3 numbers
+
+console.log("Area of Triangle abc is : "+ ((a+b+c)/2));//Area of triangle
+
+var a=2,b=3;
+console.log(b**a);
+
+var s;
+console.log(s);
+console.log(s);
+console.log(s);
+console.log(s);
+console.log(s);
---
+++ script.js
@@ -0,0 +1,12 @@
+var ar=[11,44,1,-1];
+function arlen(s)
+{
+	var len=0;
+	while(s[len]!=undefined)
+	{
+		len++;
+	}
+	return len;
+}
+
+console.log(arlen(ar));
