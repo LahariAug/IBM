1dd94c8974dc0af310b9b9117438c4cd8f5b308f
LahariAug <100933095+LahariAug@users.noreply.github.com>
2022-03-23 11:21:41 +0530

    Delete classdemo.ts

--- classdemo.ts
+++
@@ -1,9 +0,0 @@
-class Person{
-firstName : String;
-lastName: String;
-
-};
-var objectPerson = new Person();
-objectPerson.firstName = "hulk";
-console.log(objectPerson.firstName);
-console.log(objectPerson.lastName);
