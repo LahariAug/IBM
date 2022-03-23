dbd3a00f85f7ac3762d5efd3370c7e6bce6805b5
LahariAug <100933095+LahariAug@users.noreply.github.com>
2022-03-23 11:28:15 +0530

    Delete repo1.js

--- repo1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.user = void 0;
-var user = /** @class */ (function () {
-    function user() {
-    }
-    return user;
-}());
-exports.user = user;
